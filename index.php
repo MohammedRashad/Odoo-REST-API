abf565f304a432e1127b49b2ab4e2d9445e45e52
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-12-05 00:01:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once('Odoo.php');
 require_once('Customer.php');
 require_once('Sales.php');
 
-$url = 'http://meeko.no-ip.org:8069';
-$db = 'POC-DB';
-$username = 'a.reda@aurorasolutions.ca';
-$password = 'P@ssw0rd';
+$url = '';
+$db = '';
+$username = '';
+$password = '';
 
 $odoo = new Odoo($url, $db, $username, $password);
 $uid = $odoo->login();
